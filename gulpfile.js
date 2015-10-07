3a2afc4fe0ca291ab6422c0cf85bc10e7a6ea7d7
Aitor González Fernández <reimashi@gmail.com>
2015-10-07 14:39:22 +0200

    Build scripts added

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+
+var script_name = "x3map.js";
+
+gulp.task('download-deps', function (callback) {
+  var tsd = require('gulp-tsd');
+  tsd({
+        command: 'reinstall',
+        config: './tsd.json'
+    }, callback);
+});
+
+gulp.task('compilets', function () {
+    var ts = require('gulp-typescript');
+    var concat = require('gulp-concat');
+    var sourcemaps = require('gulp-sourcemaps');
+
+    var tsresult = gulp.src('script/**/*.ts')
+         .pipe(sourcemaps.init())
+         .pipe(ts({
+            target: 'ES5',
+            sortOutput: true
+         }));
+
+    return tsresult.js
+        .pipe(concat(script_name))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('public'));
+});
+
+gulp.watch('script/**/*.ts', ['compilets']);
+
+gulp.task('default', ['compilets']);
