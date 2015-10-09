e1c76ad07b736330039e9abf042d08b77ba5c5d6
Aitor González Fernández <reimashi@gmail.com>
2015-10-09 22:45:23 +0200

    Add autogenerate code for angular app

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 
-var script_name = "xut.js";
-
 gulp.task('download-deps', function (callback) {
   var tsd = require('gulp-tsd');
   tsd({
@@ -10,12 +8,12 @@ gulp.task('download-deps', function (callback) {
     }, callback);
 });
 
-gulp.task('compilets', function () {
+var compileTypescript = function(scriptname, projectdir) {
     var ts = require('gulp-typescript');
     var concat = require('gulp-concat');
     var sourcemaps = require('gulp-sourcemaps');
 
-    var tsresult = gulp.src('script/**/*.ts')
+    var tsresult = gulp.src(projectdir + '/**/*.ts')
          .pipe(sourcemaps.init())
          .pipe(ts({
             target: 'ES5',
@@ -23,11 +21,21 @@ gulp.task('compilets', function () {
          }));
 
     return tsresult.js
-        .pipe(concat(script_name))
+        .pipe(concat(scriptname))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('build'));
+}
+
+gulp.task('compilets-script', function () {
+  compileTypescript("xut.js", 'script');
 });
 
-gulp.watch('script/**/*.ts', ['compilets']);
+gulp.watch('script/**/*.ts', ['compilets-script']);
 
-gulp.task('default', ['compilets']);
+gulp.task('compilets-angular', function () {
+  compileTypescript("xut-angular.js", 'webapp/angular');
+});
+
+gulp.watch('webapp/angular/**/*.ts', ['compilets-script']);
+
+gulp.task('default', ['compilets-script', 'compilets-angular']);
